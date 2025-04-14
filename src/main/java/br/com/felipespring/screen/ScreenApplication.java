2656fdcfa0be9ec26e8d56c5a0914045fca13df7
Felipe Vieira <felipealvescoelho10@gmail.com>
2025-04-14 18:17:29 -0300

    Utilização de Streams e Coletores.

--- src/main/java/br/com/felipespring/screen/ScreenApplication.java
+++ src/main/java/br/com/felipespring/screen/ScreenApplication.java
@@ -1,11 +1,12 @@
 package br.com.felipespring.screen;
 
-import br.com.felipespring.screen.model.DadosSerie;
-import br.com.felipespring.screen.service.ConsumoApi;
-import br.com.felipespring.screen.service.ConverterDados;
+import br.com.felipespring.screen.principal.Principal;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
 
 @SpringBootApplication
 public class ScreenApplication implements CommandLineRunner {
@@ -16,14 +17,7 @@ public class ScreenApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
-        //System.out.println(json);
-		//json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
-		System.out.println(json);
-
-		ConverterDados conversor = new ConverterDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 }
